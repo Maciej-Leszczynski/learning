c7cbc63db2712e54decc8d042fbfe89a851d313f
Maciej-Leszczynski <leszczynski.m@interia.pl>
2019-10-05 16:43:54 +0200

    Finished analysis of movies with collections module

--- directors.py
+++ directors.py
@@ -1,5 +1,6 @@
 import csv
-from collections import defaultdict, namedtuple, Counter
+from collections import defaultdict, namedtuple, Counter, OrderedDict
+from operator import itemgetter
 
 MOVIE_DATA = 'movies.csv'
 NUM_TOP_DIRECTORS = 20
@@ -24,18 +25,22 @@ def get_movies_by_director():
                 continue
 
             m = Movie(title=title, year=year, score=score)
+            if m in directors[director]:
+                continue
             directors[director].append(m)
     return directors
 
-def get_average_scores(directors):
-    '''Filter directors with < MIN_MOVIES and calculate averge score'''
+def get_sorted_average_scores(directors):
+    '''Filter directors with < MIN_MOVIES and calculate averge score.
+    Return them as sorted dict'''
     aver_directors = defaultdict(list)
     for director in directors:
         movies = directors[director]
         if len(movies) >= MIN_MOVIES:
             mean = _calc_mean(movies)
-            aver_directors[director].append(mean)
-    return aver_directors
+            aver_directors[director] = mean
+    sorted_aver_directors = OrderedDict(sorted(aver_directors.items(), key=itemgetter(1), reverse=True))
+    return sorted_aver_directors
 
 def _calc_mean(movies):
     '''Helper method to calculate mean of list of Movie namedtuples'''
@@ -45,11 +50,26 @@ def _calc_mean(movies):
         score += movie.score
         count += 1
     score /= count
-    return score
+    return round(score, 1)
+
+def print_results(directors, scores):
+    '''Print directors ordered by highest average rating. For each director
+    print his/her movies also ordered by highest rated movie.'''
+    count = 0
+    for director, avg in scores.items():
+        count += 1
+        if count > NUM_TOP_DIRECTORS:
+            break
+        print(f"\n{count}. {director}: {avg}")
+        print("*" * 60)
+        for movie in directors[director]:
+            print(f"{movie.year} - {movie.title}: {movie.score} ")
+
+def main():
+    directors = get_movies_by_director()
+    scores = get_sorted_average_scores(directors)
+    print_results(directors, scores)
 
 
 if __name__ == '__main__':
-    directors = get_movies_by_director()
-    director = directors['Wes Anderson'] 
-    print(len(director))
-    print(get_average_scores(directors))
+    main()
